63de169f65428829a80bc178e16a467215d633d1
Your Name <dev.github@tpotjj.com>
2025-09-18 16:12:56 +0200

    New folder structure and fresh project yeye

--- crates/browser/src/main.rs
+++ crates/browser/src/main.rs
@@ -1,16 +1,3 @@
-mod app;
-mod renderer;
-mod egui_host;
-
-use app::{App, Wake};
-use winit::{
-    event_loop:: EventLoop,
-};
-
 fn main() {
-    let event_loop = EventLoop::<Wake>::with_user_event().build().unwrap();
-    let proxy = event_loop.create_proxy();
-    let mut app = App::new(proxy);
-    event_loop.run_app(&mut app).unwrap();
+    println!("From browser/src//main.rs");
 }
-
---
+++ src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    platform::run()
+}
